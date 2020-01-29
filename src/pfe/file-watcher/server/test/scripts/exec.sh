36db1b0621cb2acf4d97a2a04ef7a802142b1bb7
ssh24 <sakib@ibm.com>
2020-01-29 13:35:47 -0500

    Fix exec script for data dir
    Signed-off-by: ssh24 <sakib@ibm.com>

--- src/pfe/file-watcher/server/test/scripts/exec.sh
+++ src/pfe/file-watcher/server/test/scripts/exec.sh
@@ -84,6 +84,7 @@ function setup {
     TEST_LOG=$TEST_OUTPUT_DIR/$TEST_TYPE-$TEST_SUITE-test.log
     TURBINE_NPM_INSTALL_CMD="cd /file-watcher/server; npm install --only=dev"
     TURBINE_EXEC_TEST_CMD="cd /file-watcher/server; JUNIT_REPORT_PATH=/test_output.xml IMAGE_PUSH_REGISTRY_ADDRESS=${IMAGE_PUSH_REGISTRY_ADDRESS} IMAGE_PUSH_REGISTRY_NAMESPACE=${IMAGE_PUSH_REGISTRY_NAMESPACE} TURBINE_PERFORMANCE_TEST=${TURBINE_PERFORMANCE_TEST} npm run $TEST_SUITE:test:xml"
+    PERFORMANCE_TEST_DIR="mkdir -p /file-watcher/server/test/performance-test/data/$TEST_TYPE/$TURBINE_PERFORMANCE_TEST"
 
     mkdir -p $TEST_OUTPUT_DIR
 
@@ -147,9 +148,33 @@ function setup {
 
 function run {
     if [ $TEST_TYPE == "local" ]; then
+            if [ ! -z $TURBINE_PERFORMANCE_TEST ]; then
+                echo -e "${BLUE}>> Creating data directory in PFE docker if it does not exist already ... ${RESET}"
+                docker exec -i $CODEWIND_CONTAINER_ID bash -c "$PERFORMANCE_TEST_DIR"
+                checkExitCode $? "Failed to create data directory in PFE docker."
+
+                if [[ -f "$PERFORMANCE_DATA_DIR"/performance-data.json ]]; then
+                    echo -e "${BLUE}>> Copying data.json file back to docker container ... ${RESET}"
+                    docker cp "$PERFORMANCE_DATA_DIR"/performance-data.json $CODEWIND_CONTAINER_ID:/file-watcher/server/test/performance-test/data/$TEST_TYPE/$TURBINE_PERFORMANCE_TEST
+                    checkExitCode $? "Failed to copy data.json file to docker container."
+                fi
+            fi
+
         docker exec -i $CODEWIND_CONTAINER_ID bash -c "$TURBINE_EXEC_TEST_CMD" | tee $TEST_LOG
         docker cp $CODEWIND_CONTAINER_ID:/test_output.xml $TEST_OUTPUT
     elif [ $TEST_TYPE == "kube" ]; then
+            if [ ! -z $TURBINE_PERFORMANCE_TEST ]; then
+                echo -e "${BLUE}>> Creating data directory in PFE kube if it does not exist already ... ${RESET}"
+                kubectl exec -i $CODEWIND_POD_ID -- bash -c "$PERFORMANCE_TEST_DIR"
+                checkExitCode $? "Failed to create data directory in PFE kube."
+
+                if [[ -f "$PERFORMANCE_DATA_DIR"/performance-data.json ]]; then
+                    echo -e "${BLUE}>> Copying data.json file back to docker container ... ${RESET}"
+                    kubectl cp "$PERFORMANCE_DATA_DIR"/performance-data.json $CODEWIND_POD_ID:/file-watcher/server/test/performance-test/data/$TEST_TYPE/$TURBINE_PERFORMANCE_TEST
+                    checkExitCode $? "Failed to copy data.json file to docker container."
+                fi
+            fi
+
         kubectl exec -i $CODEWIND_POD_ID -- bash -c "$TURBINE_EXEC_TEST_CMD" | tee $TEST_LOG
         kubectl cp $CODEWIND_POD_ID:/test_output.xml $TEST_OUTPUT
     fi
@@ -172,11 +197,11 @@ function run {
 
     if [ $TEST_TYPE == "local" ] & [ ! -z $TURBINE_PERFORMANCE_TEST ]; then
         echo -e "${BLUE}>> Copy back data.json file back to host VM ... ${RESET}"
-        docker cp $CODEWIND_CONTAINER_ID:/file-watcher/server/test/performance-test/data/$TEST_TYPE/$TURBINE_PERFORMANCE_TEST/performance-data.json $CW_DIR/src/pfe/file-watcher/server/test/performance-test/data/$TEST_TYPE/$TURBINE_PERFORMANCE_TEST
+        docker cp $CODEWIND_CONTAINER_ID:/file-watcher/server/test/performance-test/data/$TEST_TYPE/$TURBINE_PERFORMANCE_TEST/performance-data.json "$PERFORMANCE_DATA_DIR"
         checkExitCode $? "Failed to copy data.json file to host VM local."
     elif [ $TEST_TYPE == "kube" ] & [ ! -z $TURBINE_PERFORMANCE_TEST ]; then
         echo -e "${BLUE}>> Copy back data.json file back to host VM ... ${RESET}"
-        kubectl cp $CODEWIND_POD_ID:/file-watcher/server/test/performance-test/data/$TEST_TYPE/$TURBINE_PERFORMANCE_TEST/performance-data.json $CW_DIR/src/pfe/file-watcher/server/test/performance-test/data/$TEST_TYPE/$TURBINE_PERFORMANCE_TEST
+        kubectl cp $CODEWIND_POD_ID:/file-watcher/server/test/performance-test/data/$TEST_TYPE/$TURBINE_PERFORMANCE_TEST/performance-data.json "$PERFORMANCE_DATA_DIR"
         checkExitCode $? "Failed to copy data.json file to host VM on kube."
     fi
 }
